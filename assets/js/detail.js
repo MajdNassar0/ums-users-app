557c24337b238ec207552c9c927af8d97b1cb96c
MajdNassar0 <majdmnassar05@gmail.com>
2026-02-10 13:24:31 +0200

    more details

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,6 +1,11 @@
 const getUser= async()=>{
     const parm= new URLSearchParams(window.location.search);
     const id=parm.get("id");
+     const display = `
+    <p class="id-user">Age : <span class="id">${id}</span></p>
+`;
+    document.querySelector(".id").innerHTML=display;
+
     const response =await axios.get(`https://ums12.runasp.net/api/users/${id}`);
     if(response.status==200)
         return response.data;
@@ -11,7 +16,7 @@ const detailUser =async()=>{
     const data =response.data;
 
     const display = `
-    <p class="id-user">ID : <span class="id">${data.id}</span></p>
+    <p class="id-user">Age : <span class="id">${data.age}</span></p>
     <p class="name-user">Name: <span class="name">${data.name}</span></p>
     <p class="email-user">Email : <span class="email">${data.email}</span></p>
 `;
